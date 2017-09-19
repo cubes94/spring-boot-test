5c708f9fe861e383f8f9e4f8169346b100bcdad7
cubes94 <cubes.house.94@gmail.com>
2017-09-19 15:40:47 +0800

    MongoDB用户调整

--- spring-boot-chapter10/src/main/java/com/whc/Chapter10Application.java
+++ spring-boot-chapter10/src/main/java/com/whc/Chapter10Application.java
@@ -1,15 +1,18 @@
 package com.whc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
 
 @SpringBootApplication
 public class Chapter10Application implements CommandLineRunner {
+
+    Logger LOGGER = LoggerFactory.getLogger(Chapter10Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Chapter10Application.class, args);
@@ -20,6 +23,7 @@ public class Chapter10Application implements CommandLineRunner {
     private PageProcessor githubProcessor;
 
     public void run(String... strings) throws Exception {
+        LOGGER.info("application is started ......");
 //        Spider.create(githubProcessor).addUrl("https://github.com/cubes94").thread(5).run();
     }
 }
